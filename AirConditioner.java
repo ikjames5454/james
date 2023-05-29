9fb7a65e34efee5a1952c87b55f67aa7b848b9f9
ikjames5454 <ikennajames03@gmail.com>
2023-05-29 13:20:25 +0100

    First commit

---
+++ AirConditioner.java
@@ -0,0 +1,33 @@
+ public class AirConditioner {
+     
+     private boolean isOn;
+     private String productName;
+     private int temperature;
+
+   public String airConditioner; {
+  }
+    public void decreaseTemperature() {
+     temperature = temperature;
+  }
+   public String getProductName() {
+    return productName;
+   }
+    public int getTemperature() {
+     return temperature;
+   }
+    public void increaseTemperature() {
+      temperature = temperature;
+   }
+   public boolean isOn() {
+    return isOn;
+    }
+    public void setOn(boolean isOn) {
+    this.isOn = isOn;
+   }
+    public void setProductName(String productName) {
+    this.productName = productName;
+   } 
+    public void setTemperature(int temperature) {
+     this.temperature = temperature;
+   }
+  }
